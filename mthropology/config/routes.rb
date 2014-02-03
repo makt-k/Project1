de39fdba5a9f86fd134c7f41494b0d53de573063
K Mak <karen.mak@stx.oxon.org>
2014-02-03 11:16:28 -0500

    image controller and view setup, search function specs passed

--- mthropology/config/routes.rb
+++ mthropology/config/routes.rb
@@ -1,5 +1,5 @@
 Mthropology::Application.routes.draw do
-
+  root to: 'images#show'
   resources :users do
     resources :idealogs, :expos
   end
--- mthropology/db/migrate/20140202223958_create_expos.rb
+++ mthropology/db/migrate/20140202223958_create_expos.rb
@@ -3,6 +3,7 @@ class CreateExpos < ActiveRecord::Migration
     create_table :expos do |t|
       t.text :title
       t.text :body
+      t.text :url
       t.text :tag
       t.references :user, index: true
 
