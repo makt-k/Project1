2dbf04e398976e738d2a3758625e272a786d9128
K Mak <karen.mak@stx.oxon.org>
2014-02-03 15:54:50 -0500

    set up new and show pages for idealogs

--- mthropology/app/controllers/idealogs_controller.rb
+++ mthropology/app/controllers/idealogs_controller.rb
@@ -1,2 +1,33 @@
 class IdealogsController < ApplicationController
+ before_action :get_user
+
+  def index
+    @idealogs = Idealog.all
+  end
+
+  def new
+    @idealog = Idealog.new
+  end
+
+  def create
+    @user.idealogs << Idealog.create!(idealog_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def show
+    @idealog = Idealog.find(params[:id])
+  end
+
+  private
+
+  def idealog_params
+    params.require(:idealog).permit(:title, :body, :tag, :user_id, :id, :created_at)
+  end
+
+  def get_user
+    @user= User.find(params[:user_id]) if params.key?(:user_id)
+  end
+
 end
+
+
