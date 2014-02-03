e21deb07e26b44dcc200fe0d12a9ef5c35c7b014
K Mak <karen.mak@stx.oxon.org>
2014-02-03 13:59:36 -0500

    new users controller, view, and link setup

--- mthropology/app/controllers/users_controller.rb
+++ mthropology/app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(author_params)
+    if @user.save!
+    redirect_to user_path
+    else
+    render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def author_params
+    params.require(:user).permit(:name, :email, :id)
+  end
+
 end
