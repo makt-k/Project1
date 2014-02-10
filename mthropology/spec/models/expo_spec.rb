3908ebe1956e684552de2085feb47333649467a0
K Mak <karen.mak@stx.oxon.org>
2014-02-10 12:54:35 -0500

    test for class method for Expo model passed

--- mthropology/spec/models/expo_spec.rb
+++ mthropology/spec/models/expo_spec.rb
@@ -4,5 +4,15 @@ describe Expo do
   describe "associations" do
     it { should belong_to :user }
   end
+
+  describe "GET index with serach params" do
+    before :each do
+      @expo1 = Expo.create(title: "title", body: "body", tag: "test")
+      @expo2 = Expo.create(title: "title2", body: "body2", tag: "test")
+    end
+    it "returns an array of expos with the searched tag" do
+      expect(Expo.search("test")).to eq [@expo1, @expo2]
+    end
+  end
 end
 
